a4925756207c76f094fa0c8cc9490cf2c1204b58
Fernando Guisso <fguisso@gmail.com>
2022-04-05 18:31:56 -0300

    SQUASH: get packages

--- src/installer.js
+++ src/installer.js
@@ -4,16 +4,30 @@ import * as tc from '@actions/tool-cache';
 
 const ROOT_URL = "https://github.com/OWASP/Amass/releases/download";
 
-export async function downloadAndInstall() {
+export function getPackage() {
+    switch (os.type()) {
+        case 'Windows_NT':
+            return `/amass_windows_amd64`;
+        case 'Darwin':
+            return `/amass_darwin_amd64`;
+		case 'Freebsd':
+            return `/amass_freebsd_amd64`;
+        case 'Linux':
+        default:
+            return `/amass_linux_amd64`;
+    }
+}
+
+export async function downloadAndInstall(version) {
 	const toolName = "amass";
-	const releasePackageName = "amass_linux_amd64"
-    const version = "v3.19.1"
-	
+    const latest = "v3.19.1";
+
 	core.startGroup(`Download and install Amass ${version}`);
 
-	const url = `${ROOT_URL}/${version}/${releasePackageName}.zip`;
+	const packageName = getPackage();
+	const url = `${ROOT_URL}/${version ? version : latest}/${packageName}.zip`;
 
-	core.info(`Download version ${version} from ${url}.`);
+	core.info(`Download version ${version ? version : latest} from ${url}.`);
 
 	const downloadDir = await tc.downloadTool(url);
 	if (downloadDir == null) {
@@ -25,10 +39,10 @@ export async function downloadAndInstall() {
 		throw new Error("Unable to extract Amass.");
 	}
 
-	const binPath = `${installDir}/${releasePackageName}/${toolName}`
+	const binPath = `${installDir}/${packageName}/${toolName}`
 	fs.chmodSync(binPath, "777");
 
-	core.info(`Amass ${version} was successfully installed to ${installDir}.`);
+	core.info(`Amass ${version ? version : latest} was successfully installed to ${installDir}.`);
 	core.endGroup();
 	return binPath
 }
